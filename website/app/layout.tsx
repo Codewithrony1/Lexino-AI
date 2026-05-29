8731f58c03101b346d89b23075daccb1b04de8c0
Sumit Choudhary <126940781+Codewithrony1@users.noreply.github.com>
2026-05-30 01:02:15 +0530

    update

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       signInUrl="/sign-in"
       signUpUrl="/sign-up"
       signInForceRedirectUrl="/chat"
